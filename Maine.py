ac4dd1af5f76639c4513e91ae3f58c12f09aab0f
saiwi1 <42837385+saiwi1@users.noreply.github.com>
2023-04-19 01:19:05 +0900

    restart ver

--- Maine.py
+++ Maine.py
@@ -1,5 +1,7 @@
+from csv import list_dialects
 import streamlit as st
 import datetime
+import sqlite3
 import csv
 
 st.title('治療加速&フリー加速計算')
@@ -55,16 +57,41 @@ with st.form(key='sepeed_F'):
         speed_F_total_d= speed_F_total // 1440
         if speed_F_total >= 0:
             st.text(f'{Gov_id}様  治療加速と加速合わせて、{speed_F_total_d}日分（端数切捨て)(分単位だと{speed_F_total}分です)')
-            today = datetime.date.today()
-            input_today =format(today, '%Y-%m-%d')
-            list_01=[input_today,Gov_id,speed_H_05m,speed_H_10m,speed_H_15m,speed_H_30m,speed_H_60m,speed_H_03h,speed_H_08h,speed_H_15h,speed_F_01m,speed_F_05m,speed_F_15m,speed_F_30m,speed_F_60m,speed_F_03h,speed_F_08h,speed_F_15h,speed_F_24h,speed_F_03h,speed_F_total,speed_F_total_d]
-            with open('フリー加速計算.csv', 'a') as f:
+        dt_now = datetime.datetime.now()
+        dt_str = dt_now.strftime('%Y%m%d%H%M%S')
+        db_name = 'speedup.db'
+        conn = sqlite3.connect(db_name)
+        c = conn.cursor()
+        # テーブルの作成
+        c.execute('''CREATE TABLE IF NOT EXISTS speedUP(timestamp str, year int, Month int,Day int,Hour int,Minutes int,Second int,Gov_id str,speed_F_total int,speed_F_total_d int,speed_H_05m int,speed_H_10m int,speed_H_15m int,speed_H_30m int,speed_H_60m int,speed_H_03h int,speed_H_08h int,speed_H_15h int,speed_F_01m int,speed_F_05m int,speed_F_15m int,speed_F_30m int,speed_F_60m int,speed_F_03h int,speed_F_08h int,speed_F_15h int,speed_F_24h int,speed_F_03d int)''')
+
+            #入力値を取得する
+
+        # データベースに接続する
+        conn = sqlite3.connect(db_name)
+        c = conn.cursor()
+        dt_now = datetime.datetime.now()
+        dt_str = dt_now.strftime('%Y%m%d%H%M%S')
+        dt_stY = int(dt_now.strftime('%Y'))
+        dt_stm = int(dt_now.strftime('%m'))
+        dt_std = int(dt_now.strftime('%d'))
+        dt_stH = int(dt_now.strftime('%H'))
+        dt_stM = int(dt_now.strftime('%M'))
+        dt_stS = int(dt_now.strftime('%S'))
+        # データの挿入
+        data = [
+        (dt_str,dt_stY,dt_stm,dt_std,dt_stH,dt_stM,dt_stS,Gov_id,speed_F_total,speed_F_total_d,speed_H_05m,speed_H_10m,speed_H_15m,speed_H_30m,speed_H_60m,speed_H_03h,speed_H_08h,speed_H_15h,speed_F_01m,speed_F_05m,speed_F_15m,speed_F_30m,speed_F_60m,speed_F_03h,speed_F_08h,speed_F_15h,speed_F_24h,speed_F_03d),]
+        c.executemany('INSERT INTO speedUP VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', data)
+        conn.commit()        
+        
+        data2 =[dt_str,dt_stY,dt_stm,dt_std,dt_stH,dt_stM,dt_stS,Gov_id,speed_H_05m,speed_H_10m,speed_H_15m,speed_H_30m,speed_H_60m,speed_H_03h,speed_H_08h,speed_H_15h,speed_F_01m,speed_F_05m,speed_F_15m,speed_F_30m,speed_F_60m,speed_F_03h,speed_F_08h,speed_F_15h,speed_F_24h,speed_F_03d,speed_F_total,speed_F_total_d,]
+
+        with open('speedup.csv', 'a') as f:
                 writer = csv.writer(f)
                 writer = csv.writer(f, lineterminator="\n")
-                writer.writerow(list_01)
+                writer.writerow(data2)
             
-            
-            
-            
-        else:
-            st.text(f'入力値を確認してください。ない場合は0を文字は半角で入力してください')
+
+
+    else:
+        st.text(f'入力値を確認してください。ない場合は0を文字は半角で入力してください')
